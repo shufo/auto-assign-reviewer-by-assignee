2070a5a0deaa1160d19ee6dfa852ca5e567ff551
shufo <meikyowise@gmail.com>
2020-03-12 00:36:49 +0900

    Initialize

---
+++ index.js
@@ -0,0 +1,32 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const context = github.context;
+const wait = require("./wait");
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const ms = core.getInput("milliseconds");
+    console.log(`Waiting ${ms} milliseconds ...`);
+
+    core.debug(new Date().toTimeString());
+    await wait(parseInt(ms));
+    core.debug(new Date().toTimeString());
+    console.log(ms);
+    core.setOutput("time", new Date().toTimeString());
+
+    const token = core.getInput("token");
+    const octokit = new github.GitHub(token);
+    const { data: pullRequest } = await octokit.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.pull_request.number
+    });
+
+    console.log(pullRequest);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
