46c414aaa16417d8743c522a39f4453f40522de7
shufo <meikyowise@gmail.com>
2020-04-04 17:18:53 +0900

    Update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const github = require("@actions/github");
 const context = github.context;
 const wait = require("./wait");
 const parseConfig = require("./lib/util");
+const _ = require('lodash');
+
+
+
 
 // most @actions toolkit packages have async methods
 async function run() {
@@ -17,19 +21,39 @@ async function run() {
     core.setOutput("time", new Date().toTimeString());
     console.log(context);
     const token = core.getInput("token", { required: true });
+    console.log(parseConfig());
+    const config = parseConfig();
+     
     const octokit = new github.GitHub(token);
     const { data: pullRequest } = await octokit.pulls.get({
       owner: context.repo.owner,
       repo: context.repo.repo,
-      pull_number: context.payload.pull_request.number
+      pull_number: context.payload.pull_request.number,
+      reviewers: []
+    });
+
+    const assignees = _.map(pullRequest.assignees, 'login');
+    
+    _.each(assignees, (assignee) => {
+      if (_.has(config, assignee)) {
+        let reviewers = config[assignee];
+        assignReviewers(octokit, reviewers);
+      }
     });
 
     console.log(pullRequest);
-
-    console.log(parseConfig());
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function assignReviewers(octokit, reviewers) {
+  await octokit.pulls.createReviewRequest({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.payload.pull_request.number,
+    reviewers: reviewers
+  });
 }
 
 run();
