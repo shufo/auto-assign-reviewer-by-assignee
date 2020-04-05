c10dc5367326e407fe02bffdfd0f846ceb28ca92
shufo <meikyowise@gmail.com>
2020-04-05 15:36:53 +0900

    feat(util): implement convenient utils

--- __tests__/util.test.js
+++ __tests__/util.test.js
@@ -1,10 +1,33 @@
-const { parseConfig } = require("../lib/util");
-const fs = require('fs');
+const { parseConfig, hasAssignee, getReviewers } = require("../lib/util");
+const fs = require("fs");
 
 test("config parser", async () => {
-  const content = fs.readFileSync(__basedir + '/.github/auto-assigner.yml', {
+  const content = fs.readFileSync(__basedir + "/.github/auto-assigner.yml", {
     encoding: "utf8",
   });
   const config = parseConfig(content);
   expect(config["shufo"]).toMatchObject(["shufo2"]);
+});
+
+test("assignee matching", async () => {
+  const content = fs.readFileSync(__basedir + "/.github/auto-assigner.yml", {
+    encoding: "utf8",
+  });
+
+  const config = parseConfig(content);
+
+  expect(hasAssignee(config, "shufo")).toBeTruthy();
+
+  // fallback
+  expect(hasAssignee(config, "john")).toBeTruthy();
+});
+
+test("get reviewers", async () => {
+  const content = fs.readFileSync(__basedir + "/.github/auto-assigner.yml", {
+    encoding: "utf8",
+  });
+
+  const config = parseConfig(content);
+
+  expect(getReviewers(config, "shufo")).toMatchObject(["shufo2"]);
 });
